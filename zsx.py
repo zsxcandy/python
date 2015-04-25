46170bf000997addbffd928cd12730fca6aee437
root <root@UB.(none)>
2015-04-25 10:57:23 +0800

    new file zsx.py

---
+++ zsx.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+import sys
+
+print 'The command line arguments are:'
+
+for i in sys.argv:
+	print i
+
+print '\n\nThe PYTHONPATH is', sys.path, '\n'
+print '\n\nThe PYTHONPATH is', sys.path, '\n'
+print '\n\nThe PYTHONPATH is', sys.path, '\n'
+print '\n\nThe PYTHONPATH is', sys.path, '\n'
+print '\n\nThe PYTHONPATH is', sys.path, '\n'
+print '\n\nThe PYTHONPATH is', sys.path, '\n'
